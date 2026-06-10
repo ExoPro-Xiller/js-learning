d1713a2bf19af52931f79f0555a75348f70f118b
ExoPro-Xiller <ahmad.khalid2802@gmail.com>
2026-06-10 15:13:56 +0500

    Added numbers

---
+++ DataTypes/numbers.js
@@ -0,0 +1,11 @@
+// Numbers
+let bn = 1000000000;
+let bn2 = 1_000_000_000;
+let bn3 = 1e9;
+// console.log(bn, bn2,bn3);
+// they are all the same
+
+//To produce a floating point number
+let minusnum = 1e-6;
+// console.log(minusnum);
+
