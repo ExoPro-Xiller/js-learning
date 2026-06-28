aaa588f16e683b97efe3b1ca51ff950e81d5a943
ExoPro-Xiller <ahmad.khalid2802@gmail.com>
2026-06-28 19:36:25 +0500

    arrays

---
+++ Learning/DataTypes/array.js
@@ -0,0 +1,26 @@
+let arr1 = ["apple", "mango", "strawberry"];
+// console.log(arr1[1]);
+arr1[2] = "peach";
+// console.log(arr1[2]);
+
+// console.log(arr1.at(-1));
+
+// console.log(arr1.pop());
+// console.log(arr1.shift(arr1[0]))
+
+// console.log(arr1);
+// arr1.push("orange", "kiww")
+
+// let arr2 = arr1;
+// arr2.push("fruit");
+// console.log(arr1);
+for(let fruits of arr1){
+    // console.log(fruits);
+}
+
+let user = [
+    ["user1", "address1", "info1"],
+    ["user2", "address2", "info2"],
+    ["user3", "address3", "info3"],
+]
+console.log(user[2]);
