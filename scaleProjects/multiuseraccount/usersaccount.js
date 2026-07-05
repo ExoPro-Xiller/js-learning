cbf02db705f4b0b439cdde61c736d0ac1c7059f0
ExoPro-Xiller <ahmad.khalid2802@gmail.com>
2026-07-05 16:49:15 +0500

    no duplication

--- scaleProjects/multiuseraccount/usersaccount.js
+++ scaleProjects/multiuseraccount/usersaccount.js
@@ -15,40 +15,46 @@ const confpassword = document.getElementById('confpassword');
 const loginbtn = document.getElementById('loginbtn');
 const logindashboard = document.getElementById('logindashboard');
 // TOGGLE UI
-function createaccounttoggle(){
-    if(createdashboard.style.display == "none"){
+function createaccounttoggle() {
+    if (createdashboard.style.display == "none") {
         createdashboard.style.display = "block";
-    }else createdashboard.style.display = "none";
+    } else createdashboard.style.display = "none";
 }
 
 
 // User Adding
 
-function User(userinput, emailinput,passwordinput){
+function User(userinput, emailinput, passwordinput) {
     this.Name = userinput;
     this.Email = emailinput;
     this.Password = passwordinput;
 }
 
-function createaccount(){
+function createaccount() {
 
-    if(emailinput.value == "" || userinput.value == "" || passwordinput.value == "" || confpassword.value == ""){
-        alert("Enter Complete Details");
-        return;
-    }else if(passwordinput.value != confpassword.value){
-        alert("Passwords do NOT match");
-        return;
-    }
+    for(const user of users){
+        if(user.Email === emailinput.value || user.Name === userinput.value){
+            alert("Account Already Exists");
+            return;
+        }
+    }    
+    if (emailinput.value === "" || userinput.value === "" || passwordinput.value === "" || confpassword.value === "") {
+            alert("Enter Complete Details");
+            return;
+        } else if (passwordinput.value != confpassword.value) {
+            alert("Passwords do NOT match");
+            return;
+        }
+    
 
-
-    const newUser = new User(
-        userinput.value,
-        emailinput.value,
-        passwordinput.value,
-    )
-    users.push(newUser);
-    saveUsers();
-    console.log(users);
+        const newUser = new User(
+            userinput.value,
+            emailinput.value,
+            passwordinput.value,
+        )
+        users.push(newUser);
+        saveUsers();
+        console.log(users);
 }
 
 
