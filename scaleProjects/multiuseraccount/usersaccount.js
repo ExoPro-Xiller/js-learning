c7761c74f049e7125fc96d2ad5005b2b0ef4946b
ExoPro-Xiller <ahmad.khalid2802@gmail.com>
2026-07-05 16:31:49 +0500

    added create account for multiple users

---
+++ scaleProjects/multiuseraccount/usersaccount.js
@@ -0,0 +1,55 @@
+let logstate;
+let users = JSON.parse(localStorage.getItem("users")) || [];
+
+function saveUsers() {
+    localStorage.setItem("users", JSON.stringify(users));
+}
+
+const account = document.getElementById('account');
+const createaccbtn = document.getElementById('createaccbtn');
+const createdashboard = document.getElementById('createdashboard');
+const userinput = document.getElementById('userinput');
+const emailinput = document.getElementById('emailinput');
+const passwordinput = document.getElementById('passwordinput');
+const confpassword = document.getElementById('confpassword');
+const loginbtn = document.getElementById('loginbtn');
+const logindashboard = document.getElementById('logindashboard');
+// TOGGLE UI
+function createaccounttoggle(){
+    if(createdashboard.style.display == "none"){
+        createdashboard.style.display = "block";
+    }else createdashboard.style.display = "none";
+}
+
+
+// User Adding
+
+function User(userinput, emailinput,passwordinput){
+    this.Name = userinput;
+    this.Email = emailinput;
+    this.Password = passwordinput;
+}
+
+function createaccount(){
+
+    if(emailinput.value == "" || userinput.value == "" || passwordinput.value == "" || confpassword.value == ""){
+        alert("Enter Complete Details");
+        return;
+    }else if(passwordinput.value != confpassword.value){
+        alert("Passwords do NOT match");
+        return;
+    }
+
+
+    const newUser = new User(
+        userinput.value,
+        emailinput.value,
+        passwordinput.value,
+    )
+    users.push(newUser);
+    saveUsers();
+    console.log(users);
+}
+
+
+// Authentication
